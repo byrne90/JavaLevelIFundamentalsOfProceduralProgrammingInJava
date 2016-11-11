503df4037d8f3a257b0ca48f0e2a30bc3292c82f
Byrne <Byrne@ByrneLapotp>
2016-11-11 01:03:36 +0100

    Dodanie strumienia wejścia

--- Lesson2/Home/src/Main.java
+++ Lesson2/Home/src/Main.java
@@ -1,8 +1,12 @@
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello wrold lesson project");
-	}
-
+import java.util.Scanner;
+ 
+public class Main{
+  public static void main(String[] args){
+      String imie; //w nim zapiszemy swoje imie
+      Scanner odczyt = new Scanner(System.in); // obiekt do odbierania danych przez uzytkownika
+ 
+      imie = odczyt.nextLine();
+ 
+      System.out.println("Witaj "+imie); //wyswietlamy powitanie
+  }
 }
