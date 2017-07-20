f45536acd4dae85378c87f16329f8c6028df74b4
Łukasz <ukasz@Byrne.home>
2017-07-20 20:34:05 +0200

    uzupelnienie zadanioa superfarmer

---
+++ Lesson7/SuperFarmer/src/SuperFarmerTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SuperFarmerTest {
+	// TODO: przygotuj scenariusz testowy BEFORE do testow;
+
+	@Before
+	public void setData() {
+		int rabbitCount = 28;
+		int sheepCount = 13;
+		int pigCount = 8;
+		int cowCount = 4;
+		int horseCount = 3;
+		int smallDogCount = 5;
+		int bigDogCount = 0;
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
